219295ae43de018f087f19aa55d5f1495325f5fe
ikkz <ylei.mk@gmail.com>
2025-03-20 17:14:02 +0800

    feat(input): new input template
    update
    
    refactor: use nested css
    
    update

---
+++ src/entries/input.tsx
@@ -0,0 +1,35 @@
+import { CardShell } from '@/components/card-shell';
+import { ClozeInputField } from '@/features/cloze/cloze-input-field';
+import { FIELD_ID } from '@/utils/const';
+import { isFieldEmpty } from '@/utils/field';
+import * as t from 'at/i18n';
+import { AnkiField } from 'at/virtual/field';
+import clsx from 'clsx';
+
+export default () => {
+  const hasNote = !isFieldEmpty(FIELD_ID('note'));
+  const hasAnswer = !isFieldEmpty(FIELD_ID('answer'));
+
+  return (
+    <CardShell
+      title={t.question}
+      question={<ClozeInputField name="question" />}
+      answer={
+        hasAnswer || hasNote ? (
+          <>
+            {hasAnswer ? (
+              <AnkiField name="answer" className="prose dark:prose-invert" />
+            ) : null}
+            {hasAnswer && hasNote ? <hr className="my-4" /> : null}
+            {hasNote ? (
+              <AnkiField
+                name="note"
+                className={clsx('prose prose-sm', 'dark:prose-invert')}
+              />
+            ) : null}
+          </>
+        ) : null
+      }
+    />
+  );
+};
