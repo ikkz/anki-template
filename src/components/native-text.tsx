de59bf7580568514ba78e0cf4fb44e8a68862215
ikkz <ylei.mk@gmail.com>
2025-07-15 23:37:17 +0800

    fix(match): support formula

---
+++ src/components/native-text.tsx
@@ -0,0 +1,23 @@
+import { FC, useLayoutEffect, useRef } from 'react';
+
+declare global {
+  interface Window {
+    MathJax?: {
+      typesetPromise(els: HTMLElement[]): Promise<void>;
+    };
+  }
+}
+
+export const NativeText: FC<{ text: string }> = ({ text }) => {
+  const ref = useRef<HTMLSpanElement>(null);
+
+  useLayoutEffect(() => {
+    const span = document.createElement('span');
+    span.innerText = text;
+    ref.current?.appendChild(span);
+    window.MathJax?.typesetPromise?.([span]);
+    return () => span.remove();
+  }, [text]);
+
+  return <span ref={ref} />;
+};
